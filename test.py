490a3138162ca7d06c8cdffcb53a305a2bc8dfdf
Alexey Kott <alexey.kott@gmail.com>
2017-07-30 16:40:48 +0300

    backup

--- test.py
+++ test.py
@@ -19,9 +19,15 @@ with open('data.pickle', 'rb') as f:
 
 '''
 SELECT entry.question, entry.answer
+DELETE
    FROM ftsentry                                            
    JOIN entry ON ftsentry.entry_id = entry.id               
    WHERE ftsentry MATCH 'Марс'
 
+"SELECT * 	FROM nikolay_order AS o 	JOIN nikolay_guest AS g              ON o.order_guest_id = g.guest_id             WHERE o.order_guest_company IN  (' вонделис', '')"
+
+"(' Эпам', 'FRESELLE LOGISTICS')"
+"('kabinet Õendus Juht', 'Kit Finance Europe')"
+SELECT * 	FROM nikolay_order AS o 	JOIN nikolay_guest AS g              ON o.order_guest_id = g.guest_id             WHERE o.order_guest_company IN  ('kabinet Õendus Juht', 'Kit Finance Europe')
 
 '''
